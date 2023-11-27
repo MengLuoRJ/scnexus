4601125654b838d96d4213dcbc7eabd7c8d65332
MengLuoRJ <mengluor@gmail.com>
2023-11-28 00:25:39 +0800

    feat(common): update shell

--- electron/main/common/shell.ts
+++ electron/main/common/shell.ts
@@ -2,22 +2,17 @@ import { ipcMain, shell } from "electron";
 
 export function initShellIpc() {
   ipcMain.handle("shell::open-external-website", (e, link: string) => {
-    if (!!link && (link.startsWith("https://") || link.startsWith("http://"))) {
+    if (!!link && (link.startsWith("https://") || link.startsWith("http://")))
       shell.openExternal(link);
-      return {
-        success: true,
-        message: "External website opened successfully",
-      };
-    } else {
-      return {
-        success: false,
-        message: "INVALID_LINK",
-      };
-    }
   });
   ipcMain.handle("shell::open-path", (e, path: string) => {
     if (!!path) {
       shell.openPath(path);
+    }
+  });
+  ipcMain.handle("shell::show-item-in-folder", (e, path: string) => {
+    if (!!path) {
+      shell.showItemInFolder(path);
     }
   });
 }

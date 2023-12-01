8fa7ad636cbd7567b8fb869b76b57e7ac97de354
MengLuoRJ <mengluor@gmail.com>
2023-12-02 00:30:12 +0800

    feat(dialog): update

--- src/composables/useIpcHost/useDialogIpc.ts
+++ src/composables/useIpcHost/useDialogIpc.ts
@@ -1,9 +1,11 @@
+import { OpenDialogOptions, OpenDialogSyncOptions } from "electron";
 
+const ipcRenderer = window.ipcRenderer;
 
-export async function showOpenDialogSync(options: any) {
-  const dialog = await window.ipcRenderer.invoke(
-    "dialog::show-open-dialog-sync",
-    options
-  );
-  return dialog;
+export async function showOpenDialogSync(options: OpenDialogSyncOptions) {
+  return ipcRenderer.invoke("dialog::show-open-dialog-sync", options);
+}
+
+export async function showOpenDialog(options: OpenDialogOptions) {
+  return ipcRenderer.invoke("dialog::show-open-dialog", options);
 }

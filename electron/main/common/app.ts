4bbb0e31da7ce5fbaeaac57ad18dfbd480850a41
MengLuoRJ <mengluor@gmail.com>
2023-12-08 06:03:59 +0800

    feat(app): update app ipc

--- electron/main/common/app.ts
+++ electron/main/common/app.ts
@@ -16,4 +16,14 @@ export function initAppIpc() {
   ipcMain.handle("app::get-preferred-system-languages", () => {
     return app.getPreferredSystemLanguages();
   });
+  ipcMain.handle("app::get-paths", () => {
+    return {
+      ahome: app.getPath("home"),
+      userData: app.getPath("userData"),
+      appData: app.getPath("appData"),
+      document: app.getPath("documents"),
+      logs: app.getPath("logs"),
+      crashDumps: app.getPath("crashDumps"),
+    };
+  });
 }

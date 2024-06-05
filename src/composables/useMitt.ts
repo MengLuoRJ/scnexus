93c09be9f4b89da120bf7f8d16c9d1ab77e017c2
MengLuoRJ <mengluor@gmail.com>
2024-06-05 19:20:50 +0800

    feat!: refactor Mitt composable

--- src/composables/useMitt.ts
+++ src/composables/useMitt.ts
@@ -1,18 +1,26 @@
+import { tryOnScopeDispose } from "@vueuse/core";
 import mitt from "mitt";
 
 const emitter = mitt();
 
-export function emiiterOn(type: string | symbol, handler: (evt?: any) => void) {
+function emitterOn(type: string | symbol, handler: (evt?: any) => void) {
   emitter.on(type, handler);
 }
 
-export function emiiterOff(
-  type: string | symbol,
-  handler: (evt?: any) => void
-) {
+function emitterOff(type: string | symbol, handler: (evt?: any) => void) {
   emitter.off(type, handler);
 }
 
-export function emiiterEmit(type: string | symbol, evt?: any) {
+export function emitterEmit(type: string | symbol, evt?: any) {
   emitter.emit(type, evt);
+}
+
+export function useEmitter(
+  type: string | symbol,
+  handler: (evt?: any) => void
+) {
+  tryOnScopeDispose(() => {
+    emitterOff(type, handler);
+  });
+  emitterOn(type, handler);
 }

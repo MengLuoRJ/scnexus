5c9584446c925a97450c5d7646aa192e367d0fac
MengLuoRJ <mengluor@gmail.com>
2024-06-05 18:42:01 +0800

    feat(main)!: refactor sentry

--- electron/main/utils/crash-reporter.ts
+++ electron/main/utils/crash-reporter.ts
@@ -2,6 +2,9 @@ import { crashReporter } from "electron";
 
 export function exportCrashReporter() {
   crashReporter.start({
+    companyName: "aiurcovenant",
+    productName: "scnexus",
+    ignoreSystemCrashHandler: true,
     submitURL: import.meta.env.VITE_SENTRY_SUBMIT_URL,
   });
 }
--- electron/main/utils/sentry.ts
+++ electron/main/utils/sentry.ts
@@ -1,5 +1,21 @@
-import * as Sentry from "@sentry/electron/main";
+import { init, setUser } from "@sentry/electron/main";
+import { app } from "electron";
 
 export function initSentry() {
-  Sentry.init({ dsn: import.meta.env.VITE_SENTRY_DSN });
+  if (!app.isPackaged) return;
+  init({ dsn: import.meta.env.VITE_SENTRY_DSN });
+}
+
+export function setSentryUser(user: {
+  id?: string;
+  username?: string;
+  client_id?: string;
+}) {
+  // if any field in `user` not undefined, then set the whole user `setUser(user)`,
+  // else all fields are undefined, then `setUser(null)`.
+  if (user.id || user.username || user.client_id) {
+    setUser(user);
+  } else {
+    setUser(null);
+  }
 }

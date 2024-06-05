3c39b328a3a67c10a80652d1a41585bb1f0e73e0
MengLuoRJ <mengluor@gmail.com>
2024-06-05 19:30:22 +0800

    feat!: refactor App entry

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { RouterView } from "vue-router";
-import NaiveProvider from "@/components/NaiveProvider.vue";
-import { useLocaleStore } from "@/stores/locale";
 import { set, get } from "@vueuse/core";
+import { useHead } from "@unhead/vue";
+
+import { useLocaleStore } from "@/stores/locale";
+import { useUserStore } from "./stores/user";
+
+import NaiveProvider from "@/components/NaiveProvider.vue";
+
 import { useLocale } from "@/composables/useTrans";
 import { useI18n } from "vue-i18n";
-import { useHead } from "@unhead/vue";
+import { ipcSetting } from "./apis/ipcs/setting";
+import { initUpdater } from "./composables/useUpdater";
+
 
 const localeStore = useLocaleStore();
 const locale = useLocale();
+
+const userStore = useUserStore();
 
 const { t } = useI18n();
 
@@ -20,6 +29,15 @@ useHead({
 onMounted(async () => {
   await localeStore.initLocale();
   set(locale, get(localeStore.current));
+
+  const { data: setting_client_id } = await ipcSetting.getSettingKey(
+    "CLIENT_ID"
+  );
+  await userStore.setClientId(setting_client_id as string);
+  await userStore.initUser();
+
+  await initUpdater();
+
 });
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createHead } from "@unhead/vue";
 import { createPersistedState } from "pinia-plugin-persistedstate";
-import * as Sentry from "@sentry/electron/renderer";
+import { init as sentryInit } from "@sentry/electron/renderer";
 import * as vueSentry from "@sentry/vue";
 import "virtual:uno.css";
 import "uno.css";
 
 async function bootstrap() {
   const app = createApp(App);
-
-  // setup vue-i18n
-  app.use(i18n);
 
   // setup pinia
   const pinia = createPinia();
@@ -25,33 +22,35 @@ async function bootstrap() {
   );
   app.use(pinia);
 
+  // setup vue-i18n
+  app.use(i18n);
+
   app.use(createHead());
 
   // setup router
   setupRouter(app);
   await router.isReady();
 
-  Sentry.init(
-    {
-      //@ts-ignore
-      app,
-      dsn: import.meta.env.VITE_SENTRY_DSN,
-      integrations: [new Sentry.BrowserTracing({
-        routingInstrumentation: vueSentry.vueRouterInstrumentation(router),
-      }), new Sentry.Replay()],
-      // Performance Monitoring
-      tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-    },
-    vueSentry.init
-  );
+  if (import.meta.env.MODE === "production") {
+    sentryInit(
+      {
+        dsn: import.meta.env.VITE_SENTRY_DSN,
+        integrations: [
+          vueSentry.browserTracingIntegration({ router }),
+          vueSentry.replayIntegration(),
+        ],
+        // Performance Monitoring
+        tracesSampleRate: 0.1,
+        // Session Replay
+        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+      },
+      vueSentry.init
+    );
+  }
 
   // mount when the route is ready
-  app.mount("#app").$nextTick(() => {
-    postMessage({ payload: "removeLoading" }, "*");
-  });
+  app.mount("#app");
 }
 
 bootstrap();

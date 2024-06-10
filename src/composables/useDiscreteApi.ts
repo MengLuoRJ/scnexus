45323c274e08637fc06eb17d8e4d1d9c3fa5ddb2
MengLuoRJ <mengluor@gmail.com>
2024-06-10 22:26:43 +0800

    fix!: remove locale setting in discrete apis

--- src/composables/useDiscreteApi.ts
+++ src/composables/useDiscreteApi.ts
@@ -1,41 +1,13 @@
-import { useLocaleStore } from "@/stores/locale";
 import { get } from "@vueuse/core";
 import {
   createDiscreteApi,
   lightTheme,
   darkTheme,
-  dateEnUS,
-  dateZhCN,
-  enUS,
-  zhCN,
   ConfigProviderProps,
 } from "naive-ui";
 import { computed } from "vue";
 
-const localeStore = useLocaleStore();
-const locale = get(localeStore.current);
-
 const dark = false;
-
-const naiveLocale = computed(() => {
-  switch (locale) {
-    case "zh":
-      return {
-        locale: zhCN,
-        date: dateZhCN,
-      };
-    case "en":
-      return {
-        locale: enUS,
-        date: dateEnUS,
-      };
-    default:
-      return {
-        locale: zhCN,
-        date: dateZhCN,
-      };
-  }
-});
 
 const naiveTheme = computed(() => {
   return dark ? darkTheme : lightTheme;
@@ -43,8 +15,6 @@ const naiveTheme = computed(() => {
 
 const configProviderPropsRef = computed<ConfigProviderProps>(() => ({
   theme: get(naiveTheme),
-  locale: get(naiveLocale).locale,
-  dateLocale: get(naiveLocale).date,
 }));
 
 export function useDiscreteApi<

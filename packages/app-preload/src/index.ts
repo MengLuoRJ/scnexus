fb671404f549902f2061a2eb6c13f677ff776453
MengLuoRJ <mengluor@gmail.com>
2024-09-05 05:02:00 +0800

    refactor!: refactor electron preload files to monorepo

--- packages/app-preload/src/index.ts
+++ packages/app-preload/src/index.ts
@@ -1,7 +1,9 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 const ipcRendererExposed = {
-  ...ipcRenderer,
+  invoke: async (channel: string, ...args: any[]) => {
+    return ipcRenderer.invoke(channel, ...args);
+  },
   on: (
     channel: string,
     listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void
---
+++ packages/app-preload/vite.config.ts
@@ -0,0 +1,46 @@
+import { join } from "node:path";
+import { UserConfig } from "vite";
+
+import { chrome } from "../app-main/.electron-vendors.cache.json";
+
+const PACKAGE_ROOT = __dirname;
+const PROJECT_ROOT = join(PACKAGE_ROOT, "../..");
+
+/**
+ * @type {import('vite').UserConfig}
+ * @see https://vitejs.dev/config/
+ */
+const config: UserConfig = {
+  mode: process.env.MODE,
+  root: PACKAGE_ROOT,
+  envDir: PROJECT_ROOT,
+  resolve: {
+    alias: {
+      "@/": join(PACKAGE_ROOT, "src") + "/",
+    },
+  },
+  build: {
+    ssr: true,
+    sourcemap: "inline",
+    target: `chrome${chrome}`,
+    outDir: "dist",
+    assetsDir: ".",
+    minify: process.env.MODE !== "development",
+    lib: {
+      entry: ["src/index.ts", "src/safety-index.ts"],
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["electron"],
+      output: {
+        // ESM preload scripts must have the .mjs extension
+        // https://www.electronjs.org/docs/latest/tutorial/esm#esm-preload-scripts-must-have-the-mjs-extension
+        entryFileNames: "[name].mjs",
+      },
+    },
+    emptyOutDir: true,
+    reportCompressedSize: false,
+  },
+};
+
+export default config;

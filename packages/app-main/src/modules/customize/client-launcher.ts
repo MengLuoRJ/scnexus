242a6a7d1efd0d8526d076fec4c56043fffca72e
MengLuoRJ <mengluor@gmail.com>
2024-09-30 21:48:01 +0800

    feat: add client launchers methods

--- packages/app-main/src/modules/customize/client-launcher.ts
+++ packages/app-main/src/modules/customize/client-launcher.ts
@@ -1,24 +1,45 @@
 import { profileStore } from "@/stores/profile";
 import { execFile, exec } from "child_process";
+import { GameExcutableParameters } from "@scnexus/app-shared/types/customize/client-launcher.type";
 
-export function runGameClient(path?: string) {
-  // const gameExecutable = profileStore.get("PROFILE_GAME").GAME_EXECUTABLE_X64;
+export function runGameClient() {
   const gameClient = profileStore.get("PROFILE_GAME").GAME_CLIENT;
   if (!gameClient) return;
-  if (path) {
+  // execFile(gameClient, ["-sso=1 -launch -uid s2"]);
+  // exec(`"${gameClient}" -sso=1 -launch -uid s2`);
+  exec(`"${gameClient}" -sso=1 -launch -uid s2`);
+}
+
+export function runGameExecutable(parameters?: GameExcutableParameters) {
+  const gameExecutable = profileStore.get("PROFILE_GAME").GAME_EXECUTABLE_X64;
+  if (!gameExecutable) return;
+  if (!parameters?.run) {
+    execFile(gameExecutable);
   } else {
-    // execFile(gameClient, ["-sso=1 -launch -uid s2"]);
-    // exec(`"${gameClient}" -sso=1 -launch -uid s2`);
-    exec(`"${gameClient}" -sso=1 -launch -uid s2`);
+    const command = `"${gameExecutable}" -run "${parameters.run}"`;
+    parameters.testmod && command.concat(` -testmod "${parameters.testmod}"`);
+    parameters.displaymode &&
+      command.concat(` -displaymode ${parameters.displaymode}`);
+    parameters.trigdebug && command.concat(` -trigdebug`);
+    parameters.preload && command.concat(` -preload ${parameters.preload}`);
+    parameters.NoUserCheats && command.concat(` -NoUserCheats`);
+    parameters.reloadcheck && command.concat(` -reloadcheck`);
+    parameters.meleeMod &&
+      command.concat(` -meleeMod "${parameters.meleeMod}"`);
+    parameters.difficulty &&
+      command.concat(` -difficulty ${parameters.difficulty}`);
+    parameters.speed && command.concat(` -speed ${parameters.speed}`);
+    exec(command);
   }
 }
 
-export function runEditorClient(path?: string) {
-  const editorClientPath =
+export function runEditorClient(file_path?: string) {
+  const editorExecutable =
     profileStore.get("PROFILE_GAME").GAME_EDITOR_EXECUTABLE_X64;
-  if (!editorClientPath) return;
-  if (path) {
+  if (!editorExecutable) return;
+  if (!file_path) {
+    execFile(editorExecutable);
   } else {
-    execFile(editorClientPath);
+    exec(`"${editorExecutable}" "${file_path}"`);
   }
 }
---
+++ packages/app-shared/src/types/customize/client-launcher.type.ts
@@ -0,0 +1,87 @@
+/**
+ * @see https://sc2mapster.github.io/ui/2018/02/07/testing-map-without-editor.html
+ */
+export type GameExcutableParameters = {
+  /**
+   * Runs the specified map.
+   *
+   * Map filename. Can be relative or absolute path. Relative path assumes Maps\ folder under SC2 installation folder.
+   *
+   * @type {string}
+   * @example `-run "MyMap.SC2Map"`, `-run "D:\Path\To\MyMap.SC2Map"`
+   */
+  run: string;
+  /**
+   * Mod filename. Can be relative or absolute path. Relative path assumes root SC2 installation folder.
+   * @type {string}
+   * @example `-testmod "Mods\MyMod.SC2Mod;ComponentList.SC2Components"`
+   * @example `-testmod "D:\Path\To\MyMod.SC2Mod;ComponentList.SC2Components"`
+   */
+  testmod?: string;
+  /**
+   * Sets wether SC2 should be run windowed or fullscreen
+   *
+   * `0` - Windowed
+   *
+   * `1` - Windowed Fullscreen
+   *
+   * `2` - Fullscreen
+   */
+  displaymode?: number;
+  /**
+   * Causes the Trigger Debug window to open on load finish.
+   */
+  trigdebug?: boolean;
+  /**
+   * Sets wether resources should be preloaded or not
+   *
+   * `0` - No
+   *
+   * `1` - Yes
+   */
+  preload?: number;
+  /**
+   * Unknown
+   */
+  NoUserCheats?: boolean;
+  /**
+   * Unknown
+   */
+  reloadcheck?: boolean;
+  /**
+   * Sets default Melee Mode when loading a melee map
+   *
+   * `Liberty`
+   *
+   * `Swarm`
+   *
+   * `Void`
+   */
+  meleeMod?: string;
+  /**
+   * Sets the AI difficulty
+   * 
+   * `1` - Casual
+   *
+   * `2` - Normal
+   *
+   * `3`- Hard
+   *
+   * `4`- Brutal
+   */
+  difficulty?: number;
+  /**
+   * Sets the initial game speed
+   *
+   * `0` - Slower
+   *
+   * `1` - Slow
+   *
+   * `2` - Normal
+   *
+   * `3`- Fast
+   *
+   * `4`- Faster
+   */
+  speed?: number;
+};

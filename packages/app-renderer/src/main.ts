574313c54247d8f269490aed4061bd195d485a11
MengLuoRJ <mengluor@gmail.com>
2024-12-08 04:28:09 +0800

    chore(deps): bump app renderer package dependencies

--- packages/app-renderer/src/main.ts
+++ packages/app-renderer/src/main.ts
@@ -1,37 +1,37 @@
-import App from "./App.vue";
-import router, { setupRouter } from "@/router";
-import { i18n } from "@/locales";
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { createHead } from "@unhead/vue";
-import { createPersistedState } from "pinia-plugin-persistedstate";
-import { init as sentryInit } from "@sentry/electron/renderer";
-import * as vueSentry from "@sentry/vue";
-import "virtual:uno.css";
-import "uno.css";
+import App from './App.vue'
+import router, { setupRouter } from '@/router'
+import { i18n } from '@/locales'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createHead } from '@unhead/vue'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
+import { init as sentryInit } from '@sentry/electron/renderer'
+import * as vueSentry from '@sentry/vue'
+import 'virtual:uno.css'
+import 'uno.css'
 
 async function bootstrap() {
-  const app = createApp(App);
+  const app = createApp(App)
 
   // setup pinia
-  const pinia = createPinia();
+  const pinia = createPinia()
   pinia.use(
     createPersistedState({
       key: (id) => `__pinia-presisted__${id}`,
-    })
-  );
-  app.use(pinia);
+    }),
+  )
+  app.use(pinia)
 
   // setup vue-i18n
-  app.use(i18n);
+  app.use(i18n)
 
-  app.use(createHead());
+  app.use(createHead())
 
   // setup router
-  setupRouter(app);
-  await router.isReady();
+  setupRouter(app)
+  await router.isReady()
 
-  if (import.meta.env.MODE === "production") {
+  if (import.meta.env.MODE === 'production') {
     sentryInit(
       {
         dsn: import.meta.env.VITE_SENTRY_DSN,
@@ -45,12 +45,12 @@ async function bootstrap() {
         replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
       },
-      vueSentry.init
-    );
+      vueSentry.init,
+    )
   }
 
   // mount when the route is ready
-  app.mount("#app");
+  app.mount('#app')
 }
 
-bootstrap();
+bootstrap()
--- packages/app-renderer/vite.config.ts
+++ packages/app-renderer/vite.config.ts
@@ -1,22 +1,22 @@
-import { join } from "node:path";
-import { UserConfig } from "vite";
+import { join } from 'node:path'
+import { UserConfig } from 'vite'
 
-import vue from "@vitejs/plugin-vue";
+import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from "vite-plugin-vue-devtools";
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-import UnoCSS from "unocss/vite";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
-import { unheadVueComposablesImports } from "@unhead/vue";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
-import { visualizer } from "rollup-plugin-visualizer";
+import { unheadVueComposablesImports } from '@unhead/vue'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-import { chrome } from "../app-main/.electron-vendors.cache.json";
+import { chrome } from '../app-main/.electron-vendors.cache.json'
 
-const PACKAGE_ROOT = __dirname;
-const PROJECT_ROOT = join(PACKAGE_ROOT, "../..");
+const PACKAGE_ROOT = __dirname
+const PROJECT_ROOT = join(PACKAGE_ROOT, '../..')
 
 /**
  * @type {import('vite').UserConfig}
@@ -28,10 +28,10 @@ const config: UserConfig = {
   envDir: PROJECT_ROOT,
   resolve: {
     alias: {
-      "@/": join(PACKAGE_ROOT, "src") + "/",
+      '@/': join(PACKAGE_ROOT, 'src') + '/',
     },
   },
-  base: "",
+  base: '',
   server: {
     fs: {
       strict: true,
@@ -40,25 +40,21 @@ const config: UserConfig = {
   build: {
     sourcemap: true,
     target: `chrome${chrome}`,
-    outDir: "dist",
-    assetsDir: ".",
-    minify: process.env.MODE !== "development",
+    outDir: 'dist',
+    assetsDir: '.',
+    minify: process.env.MODE !== 'development',
     rollupOptions: {
-      input: join(PACKAGE_ROOT, "index.html"),
+      input: join(PACKAGE_ROOT, 'index.html'),
       output: {
         manualChunks: {
-          vuei18n: ["vue-i18n"],
-          sentry: ["@sentry/electron", "@sentry/vue"],
-          echarts: ["echarts"],
+          vuei18n: ['vue-i18n'],
+          sentry: ['@sentry/electron', '@sentry/vue'],
+          echarts: ['echarts'],
         },
       },
     },
     emptyOutDir: true,
     reportCompressedSize: true,
-  },
-  define: {
-    __INTLIFY_JIT_COMPILATION__: true,
-    // __INTLIFY_DROP_MESSAGE_COMPILER__: true,
   },
   plugins: [
     vue(),
@@ -67,16 +63,11 @@ const config: UserConfig = {
     UnoCSS(),
     AutoImport({
       imports: [
-        "vue",
-        "vue-router",
+        'vue',
+        'vue-router',
         unheadVueComposablesImports,
         {
-          "naive-ui": [
-            "useDialog",
-            "useMessage",
-            "useNotification",
-            "useLoadingBar",
-          ],
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
         },
       ],
     }),
@@ -85,6 +76,6 @@ const config: UserConfig = {
     }),
     visualizer(),
   ],
-};
+}
 
-export default config;
+export default config

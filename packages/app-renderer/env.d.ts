477695f30aefb4b79cee973e83bcaf965a69c754
MengLuoRJ <mengluor@gmail.com>
2024-12-08 04:27:42 +0800

    chore: code clean on app renderer package

--- packages/app-renderer/env.d.ts
+++ packages/app-renderer/env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /**
+   * Sentry DSN
+   * @description Sentry DSN
+   */
+  readonly SENTRY_DSN: string
+  /**
+   * Sentry Tracing Targets
+   * @description Sentry Tracing Targets, split by `, `
+   * @default ""
+   * @example "api.example.com, example.com"
+   */
+  readonly SENTRY_TRACING_TARGETS: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
